05078fb64893e9807f634b750d6505541e253222
majed <majed@192.168.24.130>
2023-11-16 19:48:00 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import styles from '@/app/ui/home.module.css';
-
+import Image from 'next/image';
 import Link from 'next/link';
 import { lusitana } from './ui/fonts';
-import Image from 'next/image';
+
 export default function Page() {
   return (
     <main className='flex min-h-screen flex-col p-6'>
@@ -38,6 +38,13 @@ export default function Page() {
             height={760}
             className='hidden md:block'
             alt='Screenshots of the dashboard project showing desktop version'
+          />
+          <Image
+            src='/hero-mobile.png'
+            width={560}
+            height={620}
+            className='block md:hidden'
+            alt='Screenshot of the dashboard project showing mobile version'
           />
         </div>
       </div>

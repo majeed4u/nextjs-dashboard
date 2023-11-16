f9e07d03927d9a7d63dee3cdf6db67e931c950a8
majed <majed@192.168.24.130>
2023-11-16 19:09:52 +0300

    first commit

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -24,7 +24,7 @@ export default async function RevenueChart({
   // }
 
   return (
-    <div className="w-full md:col-span-4">
+    <div className='w-full md:col-span-4'>
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Recent Revenue
       </h2>
